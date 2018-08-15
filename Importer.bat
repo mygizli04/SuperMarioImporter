e573da0651b6c46216aa40029edf154bc7372e64
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2018-08-16 01:25:38 +0300

    Update Importer.bat

--- Importer.bat
+++ Importer.bat
@@ -1,11 +1,14 @@
 @echo off
 @cls
 
+:SETTINGS
+set installation=%userprofile%\Downloads 
+
 cd %userprofile%\Downloads
 del /F /Q course000
 dir | findstr .zip
 set /p x=Please er the file name : 
 "C:\Program Files\7-Zip\7z.exe" x "%x%"
 del /F /Q "%userprofile%\Desktop\cemuhook_1121_0565\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
-copy "%userprofile%\Downloads\course000" "%userprofile\Desktop\cemuhook_1121_0565\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
+copy "%installation%\course000" "%installation%\cemuhook_1121_0565\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 PAUSE
