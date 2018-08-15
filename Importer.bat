bea649d0a7b746005d70514708c91b381a43e62a
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2018-08-16 00:51:41 +0300

    Adds /F and /Q switches to del commands.

--- Importer.bat
+++ Importer.bat
@@ -6,6 +6,6 @@ del /F /Q course000
 dentir | findstr .zip
 set /p x=Please er the file name : 
 "C:\Program Files\7-Zip\7z.exe" x "%x%"
-del "%userprofile%\Desktop\cemuhook_1121_0565\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
+del /F /Q "%userprofile%\Desktop\cemuhook_1121_0565\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 copy "%userprofile%\Downloads\course000" "%userprofile\Desktop\cemuhook_1121_0565\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 PAUSE
