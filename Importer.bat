b45fa0a05500e9b5b2e72fcdd7b9f286def6ff17
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2018-08-16 01:14:36 +0300

    Corrected the typo.

--- Importer.bat
+++ Importer.bat
@@ -3,7 +3,7 @@
 
 cd %userprofile%\Downloads
 del /F /Q course000
-dentir | findstr .zip
+dir | findstr .zip
 set /p x=Please er the file name : 
 "C:\Program Files\7-Zip\7z.exe" x "%x%"
 del /F /Q "%userprofile%\Desktop\cemuhook_1121_0565\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
