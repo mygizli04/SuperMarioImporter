575cee1ee42a17958e871e48f057bbf1fe46db97
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2018-08-16 01:31:01 +0300

    Ok everything should work this time i promise :P

--- Importer.bat
+++ Importer.bat
@@ -2,13 +2,14 @@
 @cls
 
 :SETTINGS
-set installation=%userprofile%\Downloads 
+set installation=%userprofile%\Desktop\cemu
+set SearchLocation=%userprofile%\Downloads
 
-cd %userprofile%\Downloads
+cd %SearchLocation%
 del /F /Q course000
 dir | findstr .zip
 set /p x=Please er the file name : 
 "C:\Program Files\7-Zip\7z.exe" x "%x%"
-del /F /Q "%userprofile%\Desktop\cemuhook_1121_0565\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
-copy "%installation%\course000" "%installation%\cemuhook_1121_0565\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
+del /F /Q "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
+copy "%SearchLocation\course000" "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 PAUSE
