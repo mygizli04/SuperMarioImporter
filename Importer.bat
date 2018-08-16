e314011f81d8660fc353a6afeac32d3e101f95f8
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2018-08-16 13:18:29 +0300

    Now it shouldn't spam errors to the console :)

--- Importer.bat
+++ Importer.bat
@@ -10,6 +10,20 @@ del /F /Q course000
 dir | findstr .zip
 set /p x=Please enter the file name : 
 "C:\Program Files\7-Zip\7z.exe" x "%x%"
+IF NOT EXIST %SearchLocation%\course000 GOTO NOCOURSE000
 del /F /Q "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 copy "%SearchLocation%\course000" "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 PAUSE
+exit
+
+:NOCOURSE000
+ren course001 course000
+ren course002 course000
+ren course003 course000
+ren course004 course000
+ren course005 course000
+ren course006 course000
+ren course007 course000
+ren course008 course000
+ren course009 course000
+ren course010 course000
