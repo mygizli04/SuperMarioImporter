8963d9d88d0a2b37c76f794466c64726322b9bff
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2018-08-16 13:32:24 +0300

    Removed the checks, changed del to rmdir

--- Importer.bat
+++ Importer.bat
@@ -6,17 +6,10 @@ set installation=%userprofile%\Desktop\cemu
 set SearchLocation=%userprofile%\Downloads
 
 cd %SearchLocation%
-del /F /Q course000
+rmdir /F /Q course000
 dir | findstr .zip
 set /p x=Please enter the file name : 
 "C:\Program Files\7-Zip\7z.exe" x "%x%"
-IF NOT EXIST "%SearchLocation%\course000" GOTO NOCOURSE000
-del /F /Q "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
-copy "%SearchLocation%\course000" "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
-PAUSE
-exit
-
-:NOCOURSE000
 ren course001 course000
 ren course002 course000
 ren course003 course000
@@ -27,3 +20,10 @@ ren course007 course000
 ren course008 course000
 ren course009 course000
 ren course010 course000
+rmdir /F /Q "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
+copy "%SearchLocation%\course000" "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
+PAUSE
+exit
+
+:NOCOURSE000
+
