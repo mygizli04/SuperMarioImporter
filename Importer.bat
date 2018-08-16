615539877b4f8a9078a131fccae4b46f9b90c8d1
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2018-08-16 12:33:50 +0300

    typo :/

--- Importer.bat
+++ Importer.bat
@@ -8,7 +8,7 @@ set SearchLocation=%userprofile%\Downloads
 cd %SearchLocation%
 del /F /Q course000
 dir | findstr .zip
-set /p x=Please er the file name : 
+set /p x=Please enter the file name : 
 "C:\Program Files\7-Zip\7z.exe" x "%x%"
 del /F /Q "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 copy "%SearchLocation%\course000" "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
