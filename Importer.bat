d5a7e487b23180952757aefbcdbccb6b699e1cab
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2018-08-16 12:31:22 +0300

    Forgot a % there

--- Importer.bat
+++ Importer.bat
@@ -11,5 +11,5 @@ dir | findstr .zip
 set /p x=Please er the file name : 
 "C:\Program Files\7-Zip\7z.exe" x "%x%"
 del /F /Q "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
-copy "%SearchLocation\course000" "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
+copy "%SearchLocation%\course000" "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 PAUSE
