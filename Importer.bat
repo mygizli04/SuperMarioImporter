61d7c15a1f0410c93bbf964a9d76b19a3a887a94
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2018-08-16 13:28:44 +0300

    quotes -_-

--- Importer.bat
+++ Importer.bat
@@ -10,7 +10,7 @@ del /F /Q course000
 dir | findstr .zip
 set /p x=Please enter the file name : 
 "C:\Program Files\7-Zip\7z.exe" x "%x%"
-IF NOT EXIST %SearchLocation%\course000 GOTO NOCOURSE000
+IF NOT EXIST "%SearchLocation%\course000" GOTO NOCOURSE000
 del /F /Q "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 copy "%SearchLocation%\course000" "%installation%\mlc01\usr\save\00050000\1018dc00\user\80000001\course000"
 PAUSE
